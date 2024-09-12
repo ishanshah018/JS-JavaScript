0f09574cb0a5fd6d4d0949a3ba6523ee4492bdf1
Ishan Shah <153757966+ishanshah018@users.noreply.github.com>
2024-09-12 23:32:22 +0530

    prac4.js
    Program to test whether the character at the given (character)
    index is lower case.

---
+++ prac4.js
@@ -0,0 +1,10 @@
+let str="isHanshah";
+let givenindex=2;
+
+if(str[givenindex]==str[givenindex].toLowerCase)
+{
+    console.log("character is lowercase");
+}
+else{
+    console.log("character is not lowercase");
+}
