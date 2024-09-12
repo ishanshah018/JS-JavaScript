3d2dc5b3ee36b46510273a26037d6bbe54419229
Ishan Shah <153757966+ishanshah018@users.noreply.github.com>
2024-09-12 23:44:08 +0530

    Program to check if an element exists in an array or not

---
+++ prac6.js
@@ -0,0 +1,11 @@
+let arr=["ishan",22,33,-9];
+let item=33;
+
+if(arr.indexOf(item)!=-1)
+{
+    console.log("Element exists in array");
+}
+else
+{
+    console.log("Element doesn't exsts in array");
+}
