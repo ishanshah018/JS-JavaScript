e03833618096b29cdfc7266812500a48c73d2150
Ishan Shah <153757966+ishanshah018@users.noreply.github.com>
2025-03-07 10:44:32 +0530

    array_of_object.js

---
+++ Ch-8/array_of_object.js
@@ -0,0 +1,21 @@
+const carinfo=[
+
+  {
+    name:"maruti",
+    model:"swift",
+    mfg:2023
+  },
+  
+  {
+    name:"hyundai",
+    model:"venue",
+    mfg:2024
+  },
+  
+  {
+    name:"kia",
+    model:"seltos",
+    mfg:2021
+  }
+  
+]
